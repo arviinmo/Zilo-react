c30106a3911ad28038acbdfd7fdc36c36cffa7d2
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-11 17:27:46 +0330

    make complete button works

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Form = ({ inputText, setInputText, todos, setTodos }) => {
   const inputTextHandler = (e) => {
-    console.log(e.target.value);
     setInputText(e.target.value);
   };
 
