f0a4bb1a7ca16f10b1324d6070f503cdb9cf94ca
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-12 12:55:46 +0330

    add local storage feature to the app

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   };
   //Save user information to LocalStorage
   const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todo));
   };
 
   const getLocalTodos = () => {
